e156c4f497b82ef7a93d05a7639efb062ff2930c
BryCur <bryan.curchod@gmail.com>
2025-12-02 17:47:15 +0100

    specify columns for gameTitle
    Broke following the refactoring

--- src/monhundle/MonHundle.domain/Entities/DAL/GameTitle.cs
+++ src/monhundle/MonHundle.domain/Entities/DAL/GameTitle.cs
@@ -5,9 +5,18 @@ namespace MonHundle.domain.Entities.DAL;
 [Table("game_titles", Schema = "public")]
 public class GameTitle
 {
-    public int id { get; set; }
+    [Column("id")] 
+    public int Id { get; set; }
+    
+    [Column("code")]
     public required String Code { get; set; }
+    
+    [Column("name")] 
     public required String Name { get; set; }
+    
+    [Column("generation")] 
     public int Generation { get; set; }
+    
+    [Column("release_year")] 
     public int ReleaseYear { get; set; }
 }
