102d016fd678ff0b6bb35a737afa6ff888886cc1
BryCur <bryan.curchod@gmail.com>
2025-12-04 12:20:21 +0100

    Adding ping endpoint to know when the serivce is up/down

---
+++ src/monhundle/core-api/Controllers/ServiceHealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace core_api.Controllers;
+
+[ApiController]
+[Route("service")]
+public class ServiceHealthController
+{
+    [HttpGet("Status")]
+    public IActionResult GetServiceStatus()
+    {
+        return new OkObjectResult("Service is alive");
+    }
+}
