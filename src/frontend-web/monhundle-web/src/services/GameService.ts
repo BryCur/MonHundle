408083d84e33e49372a3f50df64b4d3743426d93
BryCur <bryan.curchod@gmail.com>
2026-02-05 12:26:25 +0100

    Sharing function implmented.

--- src/frontend-web/monhundle-web/src/services/GameService.ts
+++ src/frontend-web/monhundle-web/src/services/GameService.ts
@@ -46,4 +46,33 @@ export class GameService {
             return false
         })
     }
+
+    public convertGameToShareableString() : string {
+        let guessesString: string = "";
+        let guesses: Guess[] = this.gameStore.game?.guesses || [];
+
+        for(let guess of guesses) {
+            guessesString += this.getStringForResult(guess.comparisonResult.classification);
+            guessesString += this.getStringForResult(guess.comparisonResult.generation);
+            guessesString += this.getStringForResult(guess.comparisonResult.weaknesses);
+            guessesString += this.getStringForResult(guess.comparisonResult.afflictions);
+            guessesString += this.getStringForResult(guess.comparisonResult.threatLevel);
+            guessesString += this.getStringForResult(guess.comparisonResult.habitats);
+            guessesString += "\n";
+        }
+
+
+        console.log(guessesString)
+        
+        return guessesString;
+    }
+    private getStringForResult(result: ComparisonResults): string {
+        switch(result){
+            case ComparisonResults.Correct: return "🟩";
+            case ComparisonResults.Incorrect: return "🟥";
+            case ComparisonResults.Higher: return "🔺";
+            case ComparisonResults.Lower: return "🔻";
+            case ComparisonResults.Partial: return "🟨";
+        }
+    }
 }
