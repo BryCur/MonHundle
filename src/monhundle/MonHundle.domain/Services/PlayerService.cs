bc6321e90b73e85a34df206740c57a5c532880fe
BryCur <bryan.curchod@gmail.com>
2026-05-26 12:43:02 +0200

    validate, get profile, save preference and load profile endpoints

--- src/monhundle/MonHundle.domain/Services/PlayerService.cs
+++ src/monhundle/MonHundle.domain/Services/PlayerService.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.Extensions.Logging;
+using MonHundle.database.Interfaces.DataAccess;
 using MonHundle.domain.Entities.DAL;
+using MonHundle.domain.Entities.DAL.JsonStructs;
+using MonHundle.domain.Entities.DTO;
 using MonHundle.domain.Exceptions.DAL;
 using MonHundle.domain.Interfaces.DataAccess;
 using MonHundle.domain.Interfaces.Services;
 
 namespace MonHundle.domain.Services;
 
-public class PlayerService(ILogger<PlayerService> logger, IPlayerDataAccess playerDataAccess): IPlayerService
-{
+public class PlayerService(
+    ILogger<PlayerService> logger, 
+    IPlayerDataAccess playerDataAccess,
+    IGameDataAccess gameDataAccess
+): IPlayerService {
     public Guid AuthPlayer(string? playerUid)
     {
         if (playerUid is null)
@@ -37,5 +43,42 @@ public class PlayerService(ILogger<PlayerService> logger, IPlayerDataAccess play
 
             return player.PlayerUid;
         }
+    }
+
+    public bool CheckPlayerExists(Guid playerUid)
+    {
+        return playerDataAccess.GetPlayer(playerUid) != null;
+    }
+
+    public PlayerProfileResponse GetPlayerProfile(Guid playerUid)
+    {
+        Player? player = playerDataAccess.GetPlayer(playerUid);
+        if (player is null)
+        {
+            throw new DataNotFoundException($"Player {playerUid} not found");
+        }
+        
+        List<GameSession> ongoingUnlimitedGames = gameDataAccess.GetOngoingUnlimitedGamesForPlayer(playerUid);
+        GameSession? latestUnlimited = ongoingUnlimitedGames.MaxBy(gs => gs.StartTime);
+        GameSession? ongoingDaily = gameDataAccess.GetDailyGameForPlayerAtDate(DateTime.Today, player.Id!.Value);
+
+        return new PlayerProfileResponse(
+            player.JsonPreferences?.enableTableAccessibility ?? false,
+            String.Join(",", player.JsonPreferences?.gameList ?? []) ?? null,
+            ongoingDaily?.GameUid.ToString() ?? null,
+            latestUnlimited?.GameUid.ToString() ?? null
+        );
+    }
+
+    public void SaveUserPreferences(Guid playerUid, PlayerPreferencesStruct updatedPreferences)
+    {
+        Player? player = playerDataAccess.GetPlayer(playerUid);
+        if (player is null)
+        {
+            throw new DataNotFoundException($"Player {playerUid} not found");
+        }
+
+        player.JsonPreferences = updatedPreferences;
+        playerDataAccess.UpdatePlayer(player);
     }
 }
