6a7e8d5aa08f779a1a5acd5cebc593c9207887b4
Bryan Curchod <bryan.curchod@gmail.com>
2026-06-02 15:49:10 +0200

    make user_id accessible to client

--- src/monhundle/core-api/Controllers/UserController.cs
+++ src/monhundle/core-api/Controllers/UserController.cs
@@ -24,7 +24,7 @@ public class UserController(ILogger<UserController> logger, IPlayerService playe
             CookieOptions options = new CookieOptions
             {
                 MaxAge = TimeSpan.FromDays(30),
-                HttpOnly = true,
+                HttpOnly = false,
                 Secure = true,
                 SameSite = SameSiteMode.None
             };
