da699efccd10ac2a76209d9968d6e183b3d28cd9
BryCur <bryan.curchod@gmail.com>
2026-09-23 14:27:48 +0200

    clean redundant tests + dead code

--- src/frontend-web/monhundle-web/src/domain/interfaces/api-contracts/IGameApi.ts
+++ src/frontend-web/monhundle-web/src/domain/interfaces/api-contracts/IGameApi.ts
@@ -5,5 +5,4 @@ export default interface IGameApi {
     newGame: () => Promise<string>; // return game ID
     resumeGame: (gameId: string) => Promise<GameStatus | null>;
     makeGuess: (gameId:string, monsterCode: string) => Promise<GuessResponse>;
-    saveGame: (game: GameStatus) => Promise<void>;
 }
