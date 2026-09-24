8a58b029458a9cb692d220cc28dfa0ebeea6e902
BryCur <bryan.curchod@gmail.com>
2026-09-24 19:54:40 +0200

    ESLint part 5 - manual change for localSelector with part 1-4
    automatic changes was breaking the component

--- src/frontend-web/monhundle-web/src/components/LocaleSelector.vue
+++ src/frontend-web/monhundle-web/src/components/LocaleSelector.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-const { t, locale } = useI18n();
+const { locale } = useI18n();
 
 const langs = useI18n().availableLocales;
-let selectedLang = locale.value;
+const selectedLang = ref(locale.value);
 
-function switchLang(event: any) {
-    locale.value = event.target.value;
+function switchLang(event: Event) {
+    locale.value = (event.target as HTMLSelectElement).value;
 }
 </script>
 
 <template>
     <div>
         <select @change="switchLang($event)" v-model="selectedLang">
-            <option v-for="lang in langs" :value="lang">{{ lang }}</option>
+            <option v-for="lang in langs" :key="lang" :value="lang">{{ lang }}</option>
         </select>
     </div>
 </template>
